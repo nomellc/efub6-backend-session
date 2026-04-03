dd2c2e6fd7af42c30b9fee744a8d6f36b9afbb2b
nomellc <127232686+nomellc@users.noreply.github.com>
2026-04-03 16:39:02 +0900

    feat: week2 세미나 실습

--- src/main/java/com/practice/efubaccount/repository/AccountRepository.java
+++ src/main/java/com/practice/efubaccount/repository/AccountRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    boolean existsByEmail(String email);
+    Optional<Account> findByAccountId(Long accountId);
 }
