f86006645a0fbccf1b5c8dab318c41cc19c7a2f8
wys0530 <youn.soo.woo@gmail.com>
2026-04-07 23:04:05 +0900

    Revert "feat: 3주차 세션 실습 제출"
    This reverts commit 95eb16460a7c9020fa37e93aaa3daa1d5499dcdf.

--- src/main/java/com/practice/efubaccount/post/domain/Post.java
+++ src/main/java/com/practice/efubaccount/post/domain/Post.java
@@ -1,44 +1,27 @@
-package com.practice.efubaccount.post.domain;
-
-import com.practice.efubaccount.account.domain.Account;
-import com.practice.efubaccount.global.domain.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post extends BaseEntity{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Account writer;
-
-    @Column
-    private String title;
-
-    @Column
-    private String content;
-
-    @Column
-    private Long viewCount;
-
-
-    @Builder
-    public Post(String title, Account writer, String content) {
-        this.title = title;
-        this.writer = writer;
-        this.content = content;
-        this.viewCount = 0L;
-    }
-
-    public void changeContent(String newContent) {
-        this.content = newContent;
-    }
-}
+//package com.practice.efubaccount.post.domain;
+//
+//import com.practice.efubaccount.account.domain.Account;
+//import com.practice.efubaccount.global.domain.BaseEntity;
+//import jakarta.persistence.*;
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Post{
+//
+//    @Builder
+//    public Post(String title, Account writer, String content) {
+//        this.title = title;
+//        this.writer = writer;
+//        this.content = content;
+//        this.viewCount = 0L;
+//    }
+//
+//    public void changeContent(String newContent) {
+//        this.content = newContent;
+//    }
+//}
--- src/main/java/com/practice/efubaccount/post/dto/response/PostListResponse.java
+++ src/main/java/com/practice/efubaccount/post/dto/response/PostListResponse.java
@@ -1,10 +1,10 @@
-package com.practice.efubaccount.post.dto.response;
-
-import com.practice.efubaccount.post.dto.summary.PostSummary;
-
-import java.util.List;
-
-public record PostListResponse( //레코드 타입: 데이터만 포함하는 불변한 객체를 정의하는 데이터 클래스
-        List<PostSummary> posts,
-        Long totalPosts
-) {}
+//package com.practice.efubaccount.post.dto.response;
+//
+//import com.practice.efubaccount.post.dto.summary.PostSummary;
+//
+//import java.util.List;
+//
+//public record PostListResponse(
+//        List<PostSummary> posts,
+//        Long totalPosts
+//) {}
