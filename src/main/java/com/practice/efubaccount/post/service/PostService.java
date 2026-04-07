7cb3d23b4a69e442d927fbec784ebba03a1f8eaf
nomellc <127232686+nomellc@users.noreply.github.com>
2026-04-07 22:24:49 +0900

    feat: week3 세미나 실습

--- src/main/java/com/practice/efubaccount/post/service/PostService.java
+++ src/main/java/com/practice/efubaccount/post/service/PostService.java
@@ -1,7 +1,82 @@
-//package com.practice.efubaccount.post.service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class PostService {
-//
-//}
+package com.practice.efubaccount.post.service;
+
+import com.practice.efubaccount.account.domain.Account;
+import com.practice.efubaccount.account.service.AccountsService;
+import com.practice.efubaccount.global.exception.CustomException;
+import com.practice.efubaccount.global.exception.ErrorCode;
+import com.practice.efubaccount.post.domain.Post;
+import com.practice.efubaccount.post.dto.request.PostCreateRequest;
+import com.practice.efubaccount.post.dto.request.PostUpdateRequest;
+import com.practice.efubaccount.post.dto.response.PostListResponse;
+import com.practice.efubaccount.post.dto.response.PostResponse;
+import com.practice.efubaccount.post.dto.summary.PostSummary;
+import com.practice.efubaccount.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    private final AccountsService accountService;
+
+    @Transactional
+    public Long createPost(PostCreateRequest request) {
+        Account writerAccount = accountService.findByAccountId(request.getAccountId());
+
+        Post newPost = request.toEntity(writerAccount);
+        postRepository.save(newPost);
+        return newPost.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public PostListResponse getAllPosts() {
+        List<PostSummary> postSummaries = postRepository.findAllByOrderByCreatedAtDesc()
+                .stream()
+                .map(PostSummary::from).toList();
+        return new PostListResponse(postSummaries, postRepository.count());
+    }
+
+    @Transactional
+    public PostResponse getPost(Long postId) {
+        // 조회수 증가
+        postRepository.increaseViewCount(postId);
+
+        Post post = findByPostId(postId);
+        return PostResponse.from(post);
+    }
+
+    @Transactional
+    public void updatePostContent(Long postId, Long accountId, PostUpdateRequest request) {
+        Post post = findByPostId(postId);
+        Account account = accountService.findByAccountId(accountId);
+
+        authorizePostWriter(post, account);
+        post.changeContent(request.content());
+    }
+
+
+    @Transactional
+    public void deletePost(Long postId, Long accountId) {
+        Post post = findByPostId(postId);
+        Account account = accountService.findByAccountId(accountId);
+
+        authorizePostWriter(post, account);
+        postRepository.delete(post);
+    }
+
+    public Post findByPostId(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+    }
+
+    private void authorizePostWriter(Post post, Account account) {
+        if (!post.getWriter().equals(account)) {
+            throw new CustomException(ErrorCode.POST_ACCOUNT_MISMATCH);
+        }
+    }
+}
