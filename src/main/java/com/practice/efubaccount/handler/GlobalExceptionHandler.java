7a8d38ba3c0e7e84a7713a2b69c842b547fc07bb
nomellc <127232686+nomellc@users.noreply.github.com>
2026-04-14 21:08:06 +0900

    chore: GlobalExceptionHandler 주석 제거

--- src/main/java/com/practice/efubaccount/handler/GlobalExceptionHandler.java
+++ src/main/java/com/practice/efubaccount/handler/GlobalExceptionHandler.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.*;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ResponseEntity<ErrorResponseDto> handleIllegalArgument(IllegalArgumentException ex) {
-//        ErrorResponseDto error = new ErrorResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-//        return ResponseEntity.badRequest().body(error);
-//    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseDto> handleIllegalArgument(IllegalArgumentException ex) {
+        ErrorResponseDto error = new ErrorResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.badRequest().body(error);
+    }
 
 }
